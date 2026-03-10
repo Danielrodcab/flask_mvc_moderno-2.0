911e86f599bd2ca28f3e9dcc467322d3916ef285
Danielrodcab <danielrodcab97@gmail.com>
2026-03-09 20:57:11 -0500

    Rename Configuracion class to Config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-class Configuracion:
-    CLAVE_SECRETA = os.environ.get('SECRET_KEY') or 'secreto'
+class Config: 
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secreto'
     
     uri = os.environ.get('DATABASE_URL')
     if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-        
+
     SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///basededatos.db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
