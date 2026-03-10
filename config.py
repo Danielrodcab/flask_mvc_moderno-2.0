e6392d40ece11689e075c9bfb3cac32da82559fe
Danielrodcab <danielrodcab97@gmail.com>
2026-03-09 20:50:10 -0500

    Refactor Config class to Configuracion with env vars

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
-class Config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+import os
+
+class Configuracion:
+    CLAVE_SECRETA = os.environ.get('SECRET_KEY') or 'secreto'
+    
+    # Prioridad 1: Variable de Railway (DATABASE_URL)
+    # Prioridad 2: SQLite local (si no hay variable configurada)
+    uri = os.environ.get('DATABASE_URL')
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        
+    SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///basededatos.db'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
