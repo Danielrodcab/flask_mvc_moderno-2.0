e278bcbef8816b88015f8ca55eff9fca0b70469b
Danielrodcab <danielrodcab97@gmail.com>
2026-03-09 20:59:45 -0500

    Refactor app initialization and config import
    Updated app initialization to import Config directly and improved app context handling.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config 
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("config.Config")
+    app.config.from_object(Config)
 
     db.init_app(app)
 
     with app.app_context():
-
-        from app.models
+        from app import models 
+        db.create_all() 
 
     from app.routes.user_routes import user_bp
     app.register_blueprint(user_bp, url_prefix="/api/users")
